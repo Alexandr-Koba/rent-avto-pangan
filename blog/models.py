48f0e115a41d82454ae6d4070f386770af509f1f
AlexandrKoba <babakoba@yandex.ru>
2023-02-20 04:37:50 +0700

    "Commit last"

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    items = models.CharField(max_length=200)
+    descriptions = models.TextField()
+    date = models.DateField()
--- tehno/views.py
+++ tehno/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Project
 
 def home(request):
-    projects = Project.objects.all()
-    return render(request, 'tehno/home.html', {'projects': projects})
+    projects = Project.objects.all() # поместил весь список в переменную
+    return render(request, 'tehno/home.html', {'projects': projects}) # Возвращаем из запроса пользователя 
